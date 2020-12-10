ec7c682f6c5524f96a05602fbdd381026785f0db
Yvette <nchombuayvta@gmil.com>
2020-12-10 11:09:52 +0100

    context api setup

---
+++ beachrresort/src/Context.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import items from "./data"
+
+const RoomContext = React.createContext();
+
+class RoomProvider extends Component {
+  state = {
+     rooms:[],
+     sortedRooms:[],
+     featuredRooms:[],
+     loading: true
+  };
+
+  render() {
+    return (
+      <RoomContext.Provider value={{...this.state}}>
+        {this.props.children}
+      </RoomContext.Provider>
+    );
+  }
+}
+
+const RoomConsumer = RoomContext.Consumer;
+export { RoomProvider, RoomConsumer , RoomContext};
---
+++ beachrresort/src/components/FeaturedRooms.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { RoomContext } from "../Context";
+
+export default class FeaturedRooms extends Component {
+  static contextType = RoomContext;
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}

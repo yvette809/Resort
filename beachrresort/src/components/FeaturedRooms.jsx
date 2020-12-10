8de002ee12b467cdaf6ae181c09c75ac2700d192
Yvette <nchombuayvta@gmil.com>
2020-12-10 12:29:43 +0100

    featured rooms completed

--- beachrresort/src/components/FeaturedRooms.jsx
+++ beachrresort/src/components/FeaturedRooms.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import { RoomContext } from "../Context";
+import Loading from "./Loading";
+import Room from "../components/Room";
+import Title from "./Title";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map((room) => {
+      return <Room key={room.id} room={room} />;
+    });
     return (
-      <div>
-        Hello
-      </div>
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
     );
   }
 }
